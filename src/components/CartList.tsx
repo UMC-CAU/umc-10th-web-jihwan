f29d0ee85f85d59a67e0eba3a646a623c4a14122
IamInJINI <indaniel1239@gmail.com>
2026-05-31 03:17:24 +0900

    mission1

---
+++ src/components/CartList.tsx
@@ -0,0 +1,22 @@
+
+import CartItem from './Cartitem';
+import type { RootState } from '../store/store';
+import { useSelector } from '../hooks/useCustomRedux';
+
+const CartList = () => {
+    const { cartItems } = useSelector(
+        (state) => state.cart
+    );
+
+    return (
+        <div className='flex flex-col items-center justify-center'>
+            <ul>
+                {cartItems.map((item) => (
+                    <CartItem key={item.id} lp={item} />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default CartList;
---
+++ src/components/PriceBox.tsx
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "../hooks/useCustomRedux"
+import { clearCart } from "../slices/cartSlice";
+
+export const PriceBox = () => {
+    const {total} = useSelector((state)=> state.cart);
+    const dispatch = useDispatch();
+
+    const handleInitializeCart = () => {
+        dispatch(clearCart())
+    }
+  return (
+    <div className="py-12 flex justify-between">
+            <button onClick={handleInitializeCart} className="border p-4 rounded-md cursor-pointer">
+                장바구니 초기화
+            </button>
+        <div>
+            총 가격: {total}원
+        </div>
+    </div>
+  )
+}
